246ae1cdf1a90d5bfd123181b785079b6564739f
David OH <holifeilddavid@gmail.com>
2022-05-23 15:17:50 +0300

    Created Components

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/', function () {
 // });
 
 ## New School. Route Model Binding
-Route::get('/listing/{listing}', function(Listings $listing){
+Route::get('/listings/{listing}', function(Listings $listing){
     return view('listing', [
         'listing' => $listing
     ]);
